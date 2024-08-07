06c6839efb7b2c735b6122b7a1d6167967efefa0
Philix27 <philixbob@gmail.com>
2024-08-07 17:38:51 +0100

    Adjust navbar alignment

--- app/comps/navbar.tsx
+++ app/comps/navbar.tsx
@@ -1,13 +1,9 @@
-import { ThemeToggle } from "./theme-toggle"
-
 export default function Navbar() {
   return (
-    <div className="flex items-center justify-between border-b-2 border-lime-300 bg-accent px-5 h-[50px] fixed top-0 w-full">
+    <div className="border-b-2 border-lime-300 bg-accent px-5 h-[50px] fixed top-0 w-full flex items-center">
       <h1 className={"font-bold"}>
         Super<span className={`text-primary`}>Save</span>
       </h1>
-
-      <ThemeToggle />
     </div>
   )
 }
