4384b97ecba0ca51946c271e4ad29981151c329e
Philix27 <philixbob@gmail.com>
2024-08-08 23:19:26 +0100

    updated smart contract

--- app/contract/SupaSave.sol
+++ app/contract/SupaSave.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract SupaSave  {
+contract SupaSave is ReentrancyGuard {
     struct Deposit {
         uint256 amount;
         uint256 lockEndTime;
         string purpose;
     }
 
-   
+    IERC20 public cUSD;
+
+    mapping(address => Deposit[]) public deposits;
+    
+    event Deposited(address indexed user, uint256 amount, uint256 lockEndTime, string purpose);
+    event Withdrawn(address indexed user, uint256 amount);
 
     constructor(address _cUSDAddress) {
         cUSD = IERC20(_cUSDAddress);
@@ -30,10 +36,10 @@ contract SupaSave  {
             purpose: _purpose
         }));
 
-
+        emit Deposited(msg.sender, _amount, lockEndTime, _purpose);
     }
 
-    function withdraw(uint256 index) external {
+    function withdraw(uint256 index) external nonReentrant {
         require(index < deposits[msg.sender].length, "Invalid index");
         Deposit storage userDeposit = deposits[msg.sender][index];
         require(userDeposit.amount > 0, "No funds locked");
@@ -46,7 +52,7 @@ contract SupaSave  {
         // Transfer cUSD tokens from the contract to the user
         require(cUSD.transfer(msg.sender, amount), "cUSD transfer failed");
 
-
+        emit Withdrawn(msg.sender, amount);
     }
 
     function getRemainingLockTime(address user, uint256 index) external view returns (uint256) {
