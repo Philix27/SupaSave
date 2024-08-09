1d1911180cce36fdb03b810614a940ae1b01e5e4
Philix27 <philixbob@gmail.com>
2024-08-09 18:03:32 +0100

    cleanup

--- app/(pages)/withdrawal/page.tsx
+++ app/(pages)/withdrawal/page.tsx
@@ -13,29 +13,6 @@ export default function WithdrawalPage() {
   const [isLoading, setIsLoading] = useState(true)
   const { formattedBalance, userAddress } = useMain()
 
-  useEffect(() => {
-    const checkIfUserExistsAndSet = async () => {
-      if (address) {
-        // const doesUserExist = await checkIfUserExists(address)
-        // setUserExists(doesUserExist)
-        setIsLoading(false)
-      } else {
-        setIsLoading(false)
-      }
-    }
-
-    // const fetchUserByWalletAddress = async () => {
-    //   const fetchedStekcitUser = await getUserByWalletAddress(address, {
-    //     _walletAddress: address as `0x${string}`,
-    //   })
-
-    //   setSteckitUser(fetchedStekcitUser)
-    // }
-
-    checkIfUserExistsAndSet()
-    // fetchUserByWalletAddress()
-  }, [address, userExists])
-
   if (isLoading) {
     return (
       <div className="mt-[70px]">
@@ -69,10 +46,12 @@ export default function WithdrawalPage() {
           <p>Total Savings:</p>
           <p>$100</p>
         </div>
-        <div className={`mt-2 items-start justify-between`}>
-          {walletAddress && <p>{walletAddress}</p>} -
-          {address && <p>{address}</p>}
-        </div>
+
+        {userAddress && (
+          <Suspense fallback={<Loader />}>
+            <SavingsCards address={userAddress as `0x${string}`} />
+          </Suspense>
+        )}
       </div>
     </div>
   )
