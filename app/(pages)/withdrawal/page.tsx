489de0fdf5ab684b6f4d9dab359876ae7f94c3ea
Philix27 <philixbob@gmail.com>
2024-08-10 23:56:56 +0100

    addded wallet address to withdrawal page

--- app/(pages)/withdrawal/page.tsx
+++ app/(pages)/withdrawal/page.tsx
@@ -8,12 +8,8 @@ import { useAccount, useConnect } from "wagmi"
 import SavingsCards from "./Savings"
 
 export default function WithdrawalPage() {
-  const { address } = useAccount()
-  const connect = useConnect()
-  const { walletAddress, isConnected } = useMinipay()
-  const [userExists, setUserExists] = useState(false)
-  const [isLoading, setIsLoading] = useState(true)
-  const { formattedBalance, userAddress } = useMain()
+  const [isLoading] = useState(true)
+  const { userAddress } = useMain()
 
   if (isLoading) {
     return (
@@ -28,6 +24,22 @@ export default function WithdrawalPage() {
       <h1 className={`my-4 text-xl font-bold text-primary`}>Withdrawal</h1>
 
       <div className={`mt-2`}>
+        <div className={`flex items-start justify-between`}>
+          <p>Wallet Address:</p>
+          {userAddress && (
+            <p>
+              {userAddress!
+                .substring(0, 3)
+                .concat("***")
+                .concat(
+                  userAddress!.substring(
+                    userAddress!.length - 3,
+                    userAddress!.length
+                  )
+                )}
+            </p>
+          )}
+        </div>
         <div className={`mt-2 flex items-start justify-between`}>
           <p>Total Savings:</p>
           <p>$100</p>
